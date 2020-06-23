678482d9a50ebd83f129b9ea59c32f76398d2ed5
anshulrts <anshul.rts@gmail.com>
2020-06-23 17:55:37 +0530

    Children Props - We can also pass data/html tags within the Component declaration tags. - These are called children props and are rendered using props.children.

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -2,7 +2,13 @@ import React from 'react';
 
 // It is a convention to use functions with camel case notation
 const person = (props) => {
-return <p>I am {props.name} and I am {props.age} years old.</p>
+return (
+    <div>
+        <p>I am {props.name} and I am {props.age} years old.</p>
+        <p>{props.children}</p>
+    </div>
+);
+
 }
 
 export default person;
