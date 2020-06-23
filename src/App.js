236518bff6c70ec8e333501175941fb52e170409
anshulrts <anshul.rts@gmail.com>
2020-06-23 16:39:51 +0530

    Understading JSX - What we have just commented is JSX (Javascript XML). It gets compiled to Javascript objects and not HTML as it may look like. - JSX is just a sugarcoated syntax for rendering the components. The newly written code is the equivalent using react language code.

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello World!</h1>
-    </div>
-  );
+  // return (
+  //   <div className="App">
+  //     <h1>Hello World!</h1>
+  //   </div>
+  // );
+
+  return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hello World...'))
 }
 
 export default App;
