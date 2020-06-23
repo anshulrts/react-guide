863a8b3bbe44cd31440349232153fb60e0407c49
anshulrts <anshul.rts@gmail.com>
2020-06-23 16:46:43 +0530

    JSX Restrictions - It is important to know that JSX is converted to React (React.createElement) and this is converted to Javascript. - Therefore, there are few restrictions when it comes to using JSX. - For adding CSS to our div, we have used the keyword className instead of class. This is because class is a reserved keyword in JS. - Similarly, all the div and h1 etc tags that you see here belong to JSX and not HTML - Another restriction is that we must have 1 root element in JSX expression.

--- src/App.js
+++ src/App.js
@@ -3,13 +3,11 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  // return (
-  //   <div className="App">
-  //     <h1>Hello World!</h1>
-  //   </div>
-  // );
-
-  return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hello World...'))
+  return (
+    <div className="App">
+      <h1>Hello World!</h1>
+    </div>
+  );
 }
 
 export default App;
