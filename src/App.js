db0d6376ad8d2d63a329ae7aa86cb39769c2bd24
anshulrts <anshul.rts@gmail.com>
2020-06-24 23:09:04 +0530

    Revert "Using React Hooks (useState)"
    This reverts commit dd0018224a94c7ed8cfe7174b08961cea84e656e.
    
    - We are reverting this because we will be learning react with class based components as a lot of current projects are made with them.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person'
 
-const App = props => {
-  
-  const [personsState, setPersonsState] = useState({
+class App extends Component {
+  state = {
     persons: [
       {name:"Anshul", age:27},
       {name:"Kamal", age:28},
       {name:"Varun", age:25}
     ]
-  });
+  };
 
-  const switchNameHandler = () => {
-    setPersonsState({
+  switchNameHandler = () => {
+    this.setState({
       persons: [
         {name: "Max", age:29},
         {name: "Nisha", age:44},
@@ -22,15 +21,17 @@ const App = props => {
     })
   }
 
-  return (
-    <div className="App">
-      <h1>Hello World!</h1>
-      <button onClick={switchNameHandler}>Switch Name</button>
-      <Person name={personsState.persons[0].name} age={personsState.persons[0].age} />
-      <Person name={personsState.persons[1].name} age={personsState.persons[1].age} >My hobbies: Reading</Person>
-      <Person name={personsState.persons[2].name} age={personsState.persons[2].age} />
-    </div>
-  );
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello World!</h1>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} >My hobbies: Reading</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+      </div>
+    );
+  }
 }
 
 export default App;
