ba22c57303379e8d5b0965c51eecd5192494a895
anshulrts <anshul.rts@gmail.com>
2020-06-24 14:42:57 +0530

    Handling Events in a Component - Note that in HTML we have onclick, but in react we have onClick - It is customary to append the event name with 'handler' word, but not necessary. - Don't add parenthesis at the end of method name when it is declared in JSX as then it will call the function in first run.

--- src/App.js
+++ src/App.js
@@ -11,10 +11,15 @@ class App extends Component {
     ]
   };
 
+  switchNameHandler = () => {
+    console.log('Was Clicked');
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hello World!</h1>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
         <Person name={this.state.persons[1].name} age={this.state.persons[1].age} >My hobbies: Reading</Person>
         <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
