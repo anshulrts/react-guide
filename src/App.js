dd0018224a94c7ed8cfe7174b08961cea84e656e
anshulrts <anshul.rts@gmail.com>
2020-06-24 22:53:19 +0530

    Using React Hooks (useState) - Hooks are functions which allow to add state and lifecycle features to Functional Components - useState is the most important React Hook which adds state to a FC. - It returns two objects. First is the state itself which is initialised in it. Second is a function which can be used to modify this object. - One big difference between setState and useState is that the function returned by the useState to manipulate the first object overrides the values instead of merging them. Therefore we should use several useStates for different objects within a FC.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person'
 
-class App extends Component {
-  state = {
+const App = props => {
+  
+  const [personsState, setPersonsState] = useState({
     persons: [
       {name:"Anshul", age:27},
       {name:"Kamal", age:28},
       {name:"Varun", age:25}
     ]
-  };
+  });
 
-  switchNameHandler = () => {
-    this.setState({
+  const switchNameHandler = () => {
+    setPersonsState({
       persons: [
         {name: "Max", age:29},
         {name: "Nisha", age:44},
@@ -21,17 +22,15 @@ class App extends Component {
     })
   }
 
-  render() {
-    return (
-      <div className="App">
-        <h1>Hello World!</h1>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} >My hobbies: Reading</Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
-      </div>
-    );
-  }
+  return (
+    <div className="App">
+      <h1>Hello World!</h1>
+      <button onClick={switchNameHandler}>Switch Name</button>
+      <Person name={personsState.persons[0].name} age={personsState.persons[0].age} />
+      <Person name={personsState.persons[1].name} age={personsState.persons[1].age} >My hobbies: Reading</Person>
+      <Person name={personsState.persons[2].name} age={personsState.persons[2].age} />
+    </div>
+  );
 }
 
 export default App;
