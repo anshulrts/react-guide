4715694c3d4bd8a045043681a041f01555ef1da0
anshulrts <anshul.rts@gmail.com>
2020-06-24 14:34:19 +0530

    Understanding States - We can declare states only in Class based components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person'
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello World!</h1>
-      <Person name="Anshul" age="27" />
-      <Person name="Kamal" age="26" >My hobbies: Reading</Person>
-      <Person name="Varun" age="25" />
-    </div>
-  );
+class App extends Component {
+  state = {
+    persons: [
+      {name:"Anshul", age:27},
+      {name:"Kamal", age:28},
+      {name:"Varun", age:25}
+    ]
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello World!</h1>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} >My hobbies: Reading</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+      </div>
+    );
+  }
 }
 
 export default App;
