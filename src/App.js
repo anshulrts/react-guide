138262526a3617e79bfc7e4e2cccf252efb67c51
anshulrts <anshul.rts@gmail.com>
2020-06-25 22:21:15 +0530

    Conditional Rendering - In Angular, we have *ngIf. But in react, we don't have any such directive. - Therefore, we achieve conditional rendering using vanilla JS. However, we cannot use if(), so we use ternary operator. - Here, we are changing the functionality to toggle the Person component based on toggle button. - If you open the elements ab in chrome dev tools, you'll notice that the conditional div will be rendered only after clicking the button.

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ class App extends Component {
       {name:"Anshul", age:27},
       {name:"Kamal", age:28},
       {name:"Varun", age:25}
-    ]
+    ],
+    showPersons: false
   };
 
   switchNameHandler = (newName) => {
@@ -31,6 +32,11 @@ class App extends Component {
     });
   }
 
+  togglePersons = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({showPersons: !doesShow});
+  }
+
   render() {
     const style = {
       backgroundColor: 'white',
@@ -45,19 +51,26 @@ class App extends Component {
         <h1>Hello World!</h1>
         <button
           style={style}
-          onClick={() => this.switchNameHandler('Maximilan')}>
-            Switch Name
-        </button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-        <Person
-          name={this.state.persons[1].name}
-          age={this.state.persons[1].age}
-          clickPara={this.switchNameHandler.bind(this, 'Max!')}
-          changed={this.nameChanged}
+          onClick={this.togglePersons}
         >
-          My hobbies: Reading
-        </Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+            Toggle Persons
+        </button>
+        {
+          this.state.showPersons ?
+          <div>
+            <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+            <Person
+              name={this.state.persons[1].name}
+              age={this.state.persons[1].age}
+              clickPara={this.switchNameHandler.bind(this, 'Max!')}
+              changed={this.nameChanged}
+            >
+              My hobbies: Reading
+            </Person>
+            <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+          </div>
+          : null
+        }
       </div>
     );
   }
