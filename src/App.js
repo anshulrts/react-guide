8503903edcc04a5489b3f9e59ef3181e150df295
Anshul Dahiya <anshul.rts@gmail.com>
2020-06-25 15:18:29 +0530

    Adding CSS (Inline Styling) - If you want to add scoped css, using inline styling. - Note that in this case, we are using the JS equivalent of CSS properties and hence we used backgroundColor instead of background-color. Also we are wrapping the values within quotes.

--- src/App.js
+++ src/App.js
@@ -32,10 +32,22 @@ class App extends Component {
   }
 
   render() {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solic black',
+      padding: '8px',
+      cursor: 'pointer'
+    };
+
     return (
       <div className="App">
         <h1>Hello World!</h1>
-        <button onClick={() => this.switchNameHandler('Maximilan')}>Switch Name</button>
+        <button
+          style={style}
+          onClick={() => this.switchNameHandler('Maximilan')}>
+            Switch Name
+        </button>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
         <Person
           name={this.state.persons[1].name}
