50aaf4133f942a17500c8b23b9a1d2a960d76d21
anshulrts <anshul.rts@gmail.com>
2020-06-26 18:29:05 +0530

    Adding and Using Radium - When you add inline CSS using JS object, you cannot provide CSS pseudo-selector by deafult. - This is where the 3rd party library Radium comes in. Using Radium, you can specify pseudo classes even within JS.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
-import Person from './Person/Person'
+import Person from './Person/Person';
+import Radium from 'radium';
 
 class App extends Component {
   state = {
@@ -58,7 +59,11 @@ class App extends Component {
       font: 'inherit',
       border: '1px solic black',
       padding: '8px',
-      cursor: 'pointer'
+      cursor: 'pointer',
+      ':hover': {
+        backgroundColor: 'lightgreen',
+        color: 'black'
+      }
     };
 
     let persons = null;
@@ -81,6 +86,10 @@ class App extends Component {
       );
 
       style.backgroundColor = 'red';
+      style[':hover'] = {
+        backgroundColor: 'lightgreen',
+        color: 'black'
+      }
     }
 
     const classes = []
@@ -109,4 +118,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default Radium(App);
