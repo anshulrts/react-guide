ee58217a1556e01c1b724e7419f0ac4457c76f0b
anshulrts <anshul.rts@gmail.com>
2020-07-01 17:20:58 +0530

    Dynamic Styling using Styled Component - This is how we add the styles dynamically. - alt is a prop here which we pass to out component (styled.button return a component). - We can use this props directly.

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Person from './Person/Person';
 import styled from 'styled-components';
 
 const StyledButton = styled.button`
-  background-color: green;
+  background-color: ${props => props.alt ? 'red' : 'green'};
   color: white;
   font: inherit;
   border: 1px solic blue;
@@ -12,7 +12,7 @@ const StyledButton = styled.button`
   cursor: pointer;
   
   &:hover {
-  background-color: lightgreen;
+  background-color: ${props => props.alt ? 'salmon' : 'lightgreen'};
   color: black;
 }
 `;
@@ -101,6 +101,7 @@ class App extends Component {
           <h1>Hello World!</h1>
           <p className={classes.join(' ')}>This really works</p>
           <StyledButton
+            alt={this.state.showPersons}
             onClick={this.togglePersons}
           >
             Toggle Persons
