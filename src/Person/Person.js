5f382f3f9b18830af9725beecef5148cf34bd0ab
anshulrts <anshul.rts@gmail.com>
2020-07-01 22:37:14 +0530

    Applying CSS modules to Person Component

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,28 +1,14 @@
 import React from 'react';
-import './Person.css';
-import styled from 'styled-components';
-
-const StyledDiv = styled.div`
-    width: 60%;
-    margin: 16px auto;
-    border: 1px solid #eee;
-    box-shadow: 0 2px 3px #ccc;
-    padding: 16;
-    text-align: center;
-  
-  @media (min-width: 500px): {
-    width: '450px'
-  }
-  `;
+import classes from './Person.module.css'
 
 // It is a convention to use functions with camel case notation
 const person = (props) => {
     return (
-        <StyledDiv>
+        <div className={classes.Person}>
             <p onClick={props.click}>I am {props.name} and I am {props.age} years old.</p>
             <p>{props.children}</p>
             <input type="text" value={props.name} onChange={props.changed} />
-        </StyledDiv>
+        </div>
     );
 
 }
